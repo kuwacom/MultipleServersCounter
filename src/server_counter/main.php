84ad009b16daba61af2d859d12bd092f5278f9fa
DevGod6969 <85945724+DevGod6969@users.noreply.github.com>
2021-09-15 19:12:31 +0530

    English  update :)

--- src/server_counter/main.php
+++ src/server_counter/main.php
@@ -10,8 +10,8 @@ use pocketmine\utils\TextFormat as color;
 
 class main extends PluginBase implements Listener{
 	public function onEnable(){
-		$this->getLogger()->info(color::GREEN.'PlayserCounter > 新規起動時は config.yml が生成されます！');
-		$this->getLogger()->info(color::GREEN.'PlayserCounter > ステータス取得先のサーバーIP Portを設定してください！');
+		$this->getLogger()->info(color::GREEN.'PlayserCounter > config.yml will be generated!');
+		$this->getLogger()->info(color::GREEN.'Playsercounter > Set the server IP Port to get the status!');
 		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML,[
 			'PluginPrefix' => 'PlayserCounter',
 			'target_server' => [
@@ -26,34 +26,34 @@ class main extends PluginBase implements Listener{
 				],
 			'TimeOut' => 10,
 			'debug_log' => false
-			//pmmpメモ YMAL生成について
-			//ここの配列は target_serverに 1 を作りその中に ip を作り 0.0.0.0 を書き込み
-			//さらにportを作り 19132 を割り当てるという配列
-			//1 => ○○ と書いた後は , で区切って続ければ勝手に連番してくれる
+			// pmmp Memo about YMAL Generation
+			// the array here creates a 1 in target_server, creates an ip in it, writes 0.0.0.0,
+			// Further array of creating a port and assigning 19132
+			/After writing / 1=>○○, it will be serialized without permission if you keep it separated by
 		]);
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);//QueryRegenerateEvent では必要
+		$this- > getServer ()- > getPluginManager () - > registerEvents ($this, $this); / / Required by QueryRegenerateEvent
 	}
 	public function getStatus($ip, $port, $timeout){
 		$data = b"\x01\x00\x00\x00\x00L\x00\x00\x00\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x124Vx\x00\x00\x00\x00\x00\x00\x00\x00";
 
-		$sndtimeo = $timeout; // 接続タイムアウト
-		$rcvtimeo = $timeout; // 受信タイムアウト
+		$ sndtimeo = $ timeout; // connection timeout
+		$ rcvtimeo = $ timeout; // receive timeout
 		$error = 'None';
-		// ソケット作成
+		// Socket creation
 		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP );
-		// オプション設定
+		// Option Settings
 		socket_set_option( $sock, SOL_SOCKET, SO_SNDTIMEO, array("sec"=>$sndtimeo,"usec"=>0) );
 		socket_set_option( $sock, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>$rcvtimeo,"usec"=>0) );
 		
-		// 接続
+		// Connect
 		try{
-			socket_connect($sock, $ip, $port);//後々エラーで止まらないようにする
+			socket_connect ($sock, $ ip, $ port);
 		} catch(Exception $e) {
 			$error = 'error';
 		}
 
 		$error_code = socket_last_error($sock);
-		// エラーコード一覧 https://www.php.net/manual/ja/function.socket-last-error.php#95160
+		// Error code list https://www.php.net/manual/ja/function.socket-last-error.php#95160
 		if($error_code == 0){
 			$error = 'None';
 		}elseif($error_code == 110){
@@ -65,7 +65,7 @@ class main extends PluginBase implements Listener{
 		}
 		
 		if($error == "None"){
-			// 送受信
+			// Send and receive
 			$start_time = microtime(true);
 			socket_write($sock, $data);
 			$end_time = microtime(true);
@@ -75,9 +75,9 @@ class main extends PluginBase implements Listener{
 			$connect_time = ($end_time - $start_time) * 1000;
 			$rec_time = ($rec_time - $end_time) * 1000;
 			$total_time = $connect_time + $rec_time;
-			// 一応処理時間測定
+			// Once processing time measurement
 			$error_code = socket_last_error($sock);
-			// エラーコード一覧 https://www.php.net/manual/ja/function.socket-last-error.php#95160
+			// Error code list https://www.php.net/manual/ja/function.socket-last-error.php#95160
 			if($error_code == 0){
 				$error = 'None';
 			}elseif($error_code == 110){
@@ -103,11 +103,11 @@ class main extends PluginBase implements Listener{
 		$prefix = $config->get('PluginPrefix');
 		$timeout = $config->get('TimeOut');
 		$num = 0;
-		$player_count = sizeof($this->getServer()->getOnlinePlayers());// サーバー自体の接続人数
+		$ player_count = sizeof ($this - > getserver () - > Getonlineplayers ()); / / Number of connections on the server itself
 		foreach ($config->get('target_server') as $i => $nalue) {
 			$num = $num + 1;
 			if ($config->get('debug_log') == 'true') {
-			$this->getServer()->getLogger()->info(color::BLUE.'ステータス取得中...');
+			$this->getServer()->getLogger()->info(color::BLUE.'getting status...');
 			}
 			$count = main::getStatus($config->get('target_server')[$num]['ip'], $config->get('target_server')[$num]['port'],$timeout);
 			if ($config->get('debug_log') == 'true') {
@@ -116,15 +116,15 @@ class main extends PluginBase implements Listener{
 					$config->get('target_server')[$num]['ip'].
 					' port:'.
 					$config->get('target_server')[$num]['port'].
-					' 接続人数:'.
+					'Number of people connected:'.
 					$count);
-				$this->getServer()->getLogger()->info(color::GREEN.$prefix.' > '.'ステータスを取得しました！');
+				$this->getServer()->getLogger()->info(color::GREEN.$prefix.' > '.'Got the status!');
 			}
-			$player_count = $player_count + $count;// setting.yml内のサーバーの接続人数を足す
+			$player_count = $player_count + $count;// setting.Add the number of connections of servers in yml
 		}
-		$event->setPlayerCount($player_count);//接続人数変更
+		$event->setPlayerCount($player_count);//Change number of connections
 		if ($config->get('debug_log') == 'true') {
-			$this->getServer()->getLogger()->info(color::GREEN.$prefix.' > '."サーバーの接続人数を更新しました！");
+			$this->getServer()->getLogger()->info(color::GREEN.$prefix.' > '."Updated the number of connections on the server!");
 		}	
     }
 }
